1098d67739b5a22bd986506367fabbbc9fd54529
LennertDefauw <lennert.defauw@jimber.org>
2022-12-14 14:40:34 +0100

    fix: experimenting

---
+++ scripts/transfer/production-to-beta.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ENV=beta
+BETA_POD=beta-pkid-redis-5857447d74-h4wsn
+PROD_POD=redis-prod-654784cfd4-ls2f8
+
+rm ~/.kube/config
+cp ~/.kube/config-hagrid-prod-jimber ~/.kube/config
+
+kubectl cp  $PROD_POD:./ files -c redis-prod -n jimber
+
+rm ~/.kube/config
+cp ~/.kube/config-hagrid-dev-jimber ~/.kube/config
+
+kubectl cp files/ $BETA_POD:./ -c redis -n jimber
