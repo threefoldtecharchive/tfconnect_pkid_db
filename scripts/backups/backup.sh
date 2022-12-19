ad24ac90f313c81af9301f741adf4a6c14afc6d2
LennertDefauw <lennert.defauw@jimber.org>
2022-12-19 17:00:41 +0100

    fix: update scripts

--- scripts/backups/backup.sh
+++ scripts/backups/backup.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+                                                                backup.sh                                                                                #/bin/bash
 
-gsutil cp /data/dump.rdb gs://jimber_backups/pkid_redis_beta_backup/pkid_redis_backup-`date "+%d":"%m":"%y"-"%H":"%M".rdb`
+source ~/.bashrc
+
+
+/data/google-cloud-sdk/bin/gsutil cp /data/dump.rdb gs://jimber_backups/pkid_redis_beta_backup/pkid_redis_backup-$(date +\%Y\%m\%d\%H\%M\%S).rdb
+
+echo $(date)
+echo "Backup done"
--- scripts/backups/init.sh
+++ scripts/backups/init.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-apt update && apt install cron -y
+apt update && apt install python3 cron nano curl -y
 
-CLOUDSDK_CORE_DISABLE_PROMPTS=1 ./google-cloud-sdk/install.sh
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-412.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-412.0.0-linux-x86_64.tar.gz
+
+CLOUDSDK_CORE_DISABLE_PROMPTS=1 ./google-cloud-sdk/install.sh --path-update true --command-completion true
+
+source ~/.bashrc
 
 gcloud auth activate-service-account --key-file google-cloud-key.json
 chmod 741 /data/backup.sh
 
 command="/data/backup.sh"
-job="* * * * * $command"
+job="0 * * * $command"
 cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
+
+service cron start
